47079eaa7d4ec6a8d096a6887b66f2feb7385705
Jarryd Brits <jarrydbrits@gmail.com>
2021-10-21 15:26:01 +0400

    Update initdb.js

--- db/initdb.js
+++ db/initdb.js
@@ -1,22 +1,17 @@
 const mysql = require('mysql2')
-const pool = mysql.createPool({                     //MYSQL DB Configuration
-  multipleStatements: true,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'TheseusPassword',
-  database : 'theseus'
-});
-
 
 module.exports = class initdb {
 
     //CREATE SQL CONNECTION
     initialize(){
-      connection.connect(err => {
-      	if(err) throw err;
-      	console.log("Connected");
-      })
-      return connection
+      const pool = mysql.createPool({                     //MYSQL DB Configuration
+        multipleStatements: true,
+        host     : 'localhost',
+        user     : 'root',
+        password : 'TheseusPassword',
+        database : 'theseus'
+      });
+      return pool
     }
 
 
