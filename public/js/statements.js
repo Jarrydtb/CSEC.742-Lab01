7d72fa298676afa05d570d9958daeb63eabafbe1
Jarryd Brits <jarrydbrits@gmail.com>
2021-10-22 00:07:53 +0400

    Update statements.js

--- public/js/statements.js
+++ public/js/statements.js
@@ -18,6 +18,7 @@ function downloadStatement(location){
 
 function fetchStatements(){
   $.get("/api/fetch/statement_list",function(data,status){
+    console.log(data)
     data.data.forEach(element => {
       $("#statements_list").append("<li><a class='statement_button' id='"+ element.file_location +"'>"+ element.file_name +"</a></li>")
     });
