213a00dcbe1a6082f806e3f58954724fad434907
Jarryd Brits <jarrydbrits@gmail.com>
2021-10-22 00:33:17 +0400

    Update accounts.js

--- db/accounts.js
+++ db/accounts.js
@@ -21,19 +21,23 @@ module.exports = class accounts {
 
   balanceUpdate(recipient,sender,amount){
     return new Promise((resolve,reject)=>{
-      pool.execute(
-        "UPDATE accounts SET balance = balance + ? WHERE email = ?; UPDATE accounts SET balance = balance - ? WHERE email = ?;",
-        [
-          amount,
-          recipient,
-          amount,
-          sender
-        ],(err,result,fields)=>{
-          if(err){
-            reject(err)
-          }else{
-            resolve({status:200,msg:results})
-          }
+      poo.execute("UPDATE accounts SET balance = balance + ? WHERE email = ?;",[
+        amount,
+        recipient
+      ],(err,result,fields)=>{
+        if(err){reject(err)}
+        pool.execute(
+           "UPDATE accounts SET balance = balance - ? WHERE email = ?;",
+          [
+            amount,
+            sender
+          ],(err,result,fields)=>{
+            if(err){
+              reject(err)
+            }else{
+              resolve({status:200,msg:results})
+            }
+        })
       })
     })
   }
