20fa4bc035990961151c9d0a0538afab94494338
Jarryd Brits <jarrydbrits@gmail.com>
2021-10-22 14:47:59 +0400

    port update

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use(passport.session());
 
 
 
-app.listen(3000, (req,res) => console.log("Server Listening"));
+app.listen(80, (req,res) => console.log("Server Listening"));
 
 
 /* ------------------------- PAGE ROUTES ------------------------------- */
