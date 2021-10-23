8123b9570e84bee8fbbb99a40f4ac0c0c528ca39
Jarryd Brits <jarrydbrits@gmail.com>
2021-10-23 22:34:38 +0400

    Update settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  //do something
-  alert("Hello")
+  
 });
 
 
@@ -8,6 +7,6 @@ $(document).on('click','#update_button',function(req,res){
   var input = $("#inp_name").val()
   $.post('/api/update/name',{name:input},(data,status)=>{
     console.log(data.results.name);
-    $('.update_result').append($("<p>Successfully Updated to: " +data.results.name+ "</p>" ));
+    $('.update_result').append($("<p>Successfully Updated to: " + data.results.name + "</p>" ));
   });
 });
