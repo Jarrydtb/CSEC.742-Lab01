318507e93bca0de21eacfd927911bc26b6ee2cd1
Steve Sibi <steve.sibi@gmail.com>
2021-10-23 22:15:04 +0400

    Updated 'updateName' sql function inside 'users.js'

--- db/users.js
+++ db/users.js
@@ -1,66 +1,74 @@
 const mysql = require('mysql2')
 const pool = require('./initdb.js')
 
-module.exports =  class users {
+module.exports = class users {
 
-  userFindByEmail(value){
-    return new Promise((resolve,reject)=>{
-      pool.execute("SELECT * FROM `users` WHERE `email` = ?",[
+  userFindByEmail(value) {
+    return new Promise((resolve, reject) => {
+      pool.execute("SELECT * FROM `users` WHERE `email` = ?", [
         value,    // example@example.com etc...
-      ],(err,results,fields)=>{
-        if(err){
+      ], (err, results, fields) => {
+        if (err) {
           reject(err)
-        }else{
-          resolve({status:200,results})
+        } else {
+          resolve({ status: 200, results })
         }
       })
     })
   }
 
-  userFindById(value){
-    return new Promise((resolve,reject)=>{
-      pool.execute("SELECT * FROM `users` WHERE `id` = ?",[
+  userFindById(value) {
+    return new Promise((resolve, reject) => {
+      pool.execute("SELECT * FROM `users` WHERE `id` = ?", [
         value,    // example@example.com etc...
-      ],(err,results,fields)=>{
-        if(err){
+      ], (err, results, fields) => {
+        if (err) {
           reject(err)
-        }else{
-          resolve({status:200,results})
+        } else {
+          resolve({ status: 200, results })
         }
       })
     })
   }
 
-  addNew(name, email, password){
-    return new Promise((resolve,reject)=>{
-      pool.execute("INSERT INTO users (name, email, password, account_type) VALUES(?, ?, ?, 'customer')",[
+  addNew(name, email, password) {
+    return new Promise((resolve, reject) => {
+      pool.execute("INSERT INTO users (name, email, password, account_type) VALUES(?, ?, ?, 'customer')", [
         name,
         email,
         password
-      ],(err,results)=>{
-        if(err){
+      ], (err, results) => {
+        if (err) {
           reject(err)
-        }else{
-          resolve({status:200,results})
+        } else {
+          resolve({ status: 200, results })
         }
       })
     })
   }
 
-  updateName(name,email){
-    return new Promise((resolve,reject)=>{
-      pool.execute("UPDATE users SET name = ? WHERE email= ?; SELECT name FROM users WHERE email = ?;",[
-          name,
-          email,
-          email
-      ], (err, results)=> {
-        if(err){
+  updateName(name, email) {
+    return new Promise((resolve, reject) => {
+      pool.execute("UPDATE users SET name = ? WHERE email= ?;", [
+        name,
+        email,
+      ], (err, results, fields) => {
+        if (err) {
           reject(err)
-        }else{
-          resolve({status:200,results})
         }
+        pool.execute(
+          " SELECT name FROM users WHERE email = ?;"
+          [
+          email
+          ], (err, results, fields) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve({ status: 200, results })
+            }
+          })
       })
     })
   }
-  
+
 }
