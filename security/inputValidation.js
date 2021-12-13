41c9d6e8b47b6e8dc64f171a05863e0a4648d945
Steve Sibi <steve.sibi@gmail.com>
2021-12-13 21:37:23 +0400

    Have updated validatePassword function to match password policies

--- security/inputValidation.js
+++ security/inputValidation.js
@@ -2,66 +2,73 @@ const bcrypt = require('bcrypt');
 
 module.exports = class inputValidation {
 
-  validateEmail(email){//returns true or false
+  validateEmail(email) {//returns true or false
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
-  validatePassword(passwordString){//returns hashed password
-    return new Promise((resolve,reject)=>{
-      bcrypt
-        	 .hash(passwordString,10)
-           .then(hashed =>{resolve(hashed)})
-           .catch(err =>{reject(err)})
-    })
-  }
-  validateString(string){//validates input string
-    const re = /^(?!\s*$).+/;
-    return re.test(String(string))
-  }
 
-  validatePath(id,path){
-    if(path.split('/')[3]==id){
-      // /user_data/[user id]/[file name]                     -> VALID
-      // /user_data/[user id]/../[other user id]/[file name]  -> INVALID
-      const re = /^(?:\/[^\/]+(?!\.\.))+\/[^\/]+$/;
-      return re.test(String(path).toLowerCase());
-    }else{
+  validatePassword(passwordString) {//returns hashed password
+    const re = /^(?=.[0-9])(?=.[!@#$%^&])[a-zA-Z0-9!@#$%^&]{6,16}$/;
+    if (re.test(String(passwordString).toLowerCase()) == true) {
+      return new Promise((resolve, reject) => {
+        bcrypt
+          .hash(passwordString, 10)
+          .then(hashed => { resolve(hashed) })
+          .catch(err => { reject(err) })
+      })
+    } else {
       return false
     }
   }
 
-  validateTransferAmount(email,amount){
-    return new Promise((resolve,reject)=>{
+  validateString(string) {//validates input string
+    const re = /^(?!\s*$).+/;
+    return re.test(String(string))
+  }
+
+  validatePath(id, path) {
+    if (path.split('/')[3] == id) {
+      // /user_data/[user id]/[file name]                     -> VALID
+      // /user_data/[user id]/../[other user id]/[file name]  -> INVALID
+      const re = /^(?:\/[^\/]+(?!\.\.))+\/[^\/]+$/;
+      return re.test(String(path).toLowerCase());
+    } else {
+      return false
+    }
+  }
+
+  validateTransferAmount(email, amount) {
+    return new Promise((resolve, reject) => {
       var accountsDB = require('../db/accounts.js')
       var Account = new accountsDB()
       Account.getBalance(email)
-      .then(data=>{
-        console.log(data.data)
-        if(amount>0 && amount <= data.data.balance){
-          resolve(true)
-        }else{
-          console.log("Illegal Operation")
+        .then(data => {
+          console.log(data.data)
+          if (amount > 0 && amount <= data.data.balance) {
+            resolve(true)
+          } else {
+            console.log("Illegal Operation")
+            reject(false)
+          }
+        })
+        .catch(err => {
           reject(false)
-        }
-      })
-      .catch(err=>{
-        reject(false)
-      })
+        })
     })
   }
 
-  validateTransferRecipient(selfEmail, recipientEmail){
-    return new Promise((resolve,reject)=>{
-      if(selfEmail!==recipientEmail && this.validateEmail(recipientEmail)){
+  validateTransferRecipient(selfEmail, recipientEmail) {
+    return new Promise((resolve, reject) => {
+      if (selfEmail !== recipientEmail && this.validateEmail(recipientEmail)) {
         //Check valid email
         var userDB = require('../db/users.js')
         var User = new userDB()
         User.userFindByEmail(recipientEmail)
-        .then(data=>{
-          if(data.results.length>0){resolve(true)}else{reject(false)}
-        })
-        .catch(err=>{reject(false)})
-      }else{
+          .then(data => {
+            if (data.results.length > 0) { resolve(true) } else { reject(false) }
+          })
+          .catch(err => { reject(false) })
+      } else {
         reject(false)
       }
     })
